a84aaf9c3163eda4f82b54e2d342643eeb0156dc
Corey Barr <corey.jbarr@gmail.com>
2020-02-11 16:38:24 -0600

    database pool set up

--- server/server.js
+++ server/server.js
@@ -2,12 +2,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const pg = require('pg');
 //uses
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 //globals
 const port = 5000;
+
 //db setup
+const pool = new pg.Pool({
+    database: 'music_library',
+    host: 'localhost',
+    port: 5432,
+    max:12,
+    idleTimeoutMillis:30000
+}); //end pool
 
 //server up
 app.listen(port,()=>{
