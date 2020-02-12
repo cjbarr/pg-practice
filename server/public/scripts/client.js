def265d50ccc9d91d19bd7717549e525e7e2face
Corey Barr <corey.jbarr@gmail.com>
2020-02-12 10:25:16 -0600

    get ajax on client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,19 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('yoo');
+    getSongs();
 }
+
+function getSongs(){
+    //make an ajax request
+    $.ajax({
+        type:'GET',
+        url:'songs'
+    }).then(function(response){
+        console.log('back from get with', response);
+
+    }).catch(function(err){
+        console.log(err);
+        alert('not working');
+    })//end ajax
+}//end get
