3762e9c0f539b8970ce94d830ba60c7438815fab
Corey Barr <corey.jbarr@gmail.com>
2020-02-13 10:43:07 -0600

    up and down put buttons and functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -27,9 +27,47 @@ function onReady(){
     console.log('yoo');
     getSongs();
     $('#addSongButton').on('click', addSong);
-    $('#displaySpot').on('click', '.deleteButton', deleteSong)
+    $('#displaySpot').on('click', '.deleteButton', deleteSong);
+    $('#displaySpot').on('click', '.upVoteButton', upVote);
+    $('#displaySpot').on('click', '.downVoteButton', downVote);
 }
 
+
+function upVote(){
+    let selectedId = $(this).parent().data('id');
+    console.log(selectedId);
+      $.ajax({
+                  type: 'PUT',
+                  url: `/songs/${selectedId}`,
+                  data:{
+                       voteDirection:'up'
+                  }
+            }).then(function (response) {
+                console.log('back from PUT with', response);
+                getSongs();
+            }).catch(function (err) {
+                console.log(err);
+                alert('PUT not working');
+            }) //end ajax
+        }
+
+function downVote(){
+      let selectedId = $(this).parent().data('id');
+      console.log(selectedId);
+      $.ajax({
+          type: 'PUT',
+          url: `/songs/${selectedId}`,
+          data: {
+              voteDirection: 'down'
+          }
+      }).then(function (response) {
+          console.log('back from PUT with', response);
+          getSongs();
+      }).catch(function (err) {
+          console.log(err);
+          alert('PUT not working');
+      }) //end ajax
+}
 
 function deleteSong(){
 let selectedId = $(this).parent().data('id');
@@ -66,6 +104,6 @@ function displaySongs(responseArray){
     let el = $('#displaySpot');
     el.empty();
     for(i=0;i<responseArray.length;i++){
-        el.append(`<li data-id=${responseArray[i].id}>(${responseArray[i].id})${responseArray[i].artist}<br> ${responseArray[i].track}<button class='deleteButton'>Delete</button></li><br><br>`)
+        el.append(`<li data-id=${responseArray[i].id}>(${responseArray[i].rank})${responseArray[i].artist}<br> ${responseArray[i].track}<br><button class='deleteButton'>Delete</button><br><button class="upVoteButton">Up</button><button class="downVoteButton">Down</button></li><br><br>`)
     }
 }
--- server/server.js
+++ server/server.js
@@ -5,60 +5,84 @@ const app = express();
 const pg = require('pg');
 //uses
 app.use(express.static('server/public'));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 //globals
 const port = 5000;
 
 //db setup
 const pool = new pg.Pool({
-    database: 'music_library',
-    host: 'localhost',
-    port: 5432,
-    max:12,
-    idleTimeoutMillis:30000
+  database: 'music_library',
+  host: 'localhost',
+  port: 5432,
+  max: 12,
+  idleTimeoutMillis: 30000
 }); //end pool
 
 //server up
-app.listen(port,()=>{
-    console.log('server up on', port)
+app.listen(port, () => {
+  console.log('server up on', port)
 });
 //routes
-app.get('/songs', (req,res)=>{
-    console.log('in songs GET');
-    //set up a query
-    let queryString = 'SELECT * FROM songs';
-    //try to run query on our pool
-    pool.query(queryString).then((results)=>{
-        res.send(results.rows);
-    //if successful we'll send a response with rows from results
-      }).catch((err)=>{
-    //catch any errors
-      console.log(err);
-    res.sendStatus(500);
-})
-   
-})//end /songs GET
-
-
-app.delete('/songs/:id', (req,res)=>{
-  console.log('in /songs delete', req.params.id);
-  let queryString = `DELETE FROM songs WHERE "id"=${req.params.id}`;
+app.get('/songs', (req, res) => {
+  console.log('in songs GET');
+  //set up a query
+  let queryString = 'SELECT * FROM songs ORDER BY "rank" ASC';
+  //try to run query on our pool
   pool.query(queryString).then((results) => {
-res.send(results.rows);
+    res.send(results.rows);
+    //if successful we'll send a response with rows from results
+  }).catch((err) => {
+    //catch any errors
+    console.log(err);
+    res.sendStatus(500);
+  })
+
+}) //end /songs GET
+
+app.put('/songs/:id', (req, res) => {
+  console.log('in /songs PUT', req.params.id, req.body );
+  if (req.body.voteDirection == 'up'){
+        let queryString = `UPDATE songs SET rank = rank-1 WHERE "id"=${req.params.id}`;
+          pool.query(queryString).then((results) => {
+                res.sendStatus(200);
+    }).catch((err) => {
+          console.log(err);
+          res.sendStatus(500);
+    })
+  }
+else if (req.body.voteDirection == 'down'){
+  let queryString = `UPDATE songs SET rank = rank+1 WHERE "id"=${req.params.id}`;
+    pool.query(queryString).then((results) => {
+          res.sendStatus(200);
 }).catch((err) => {
-      //catch any errors
       console.log(err);
       res.sendStatus(500);
 })
+}
+  })
+
+
+app.delete('/songs/:id', (req, res) => {
+  console.log('in /songs delete', req.params.id);
+  let queryString = `DELETE FROM songs WHERE "id"=$1`;
+  pool.query(queryString, [req.params.id]).then((results) => {
+    res.send(results.rows);
+  }).catch((err) => {
+    //catch any errors
+    console.log(err);
+    res.sendStatus(500);
+  })
 })
 
-app.post('/songs', (req,res)=>{
+app.post('/songs', (req, res) => {
   console.log('in songs POST', req.body);
   let queryString = 'INSERT INTO songs("rank","artist","track","published") VALUES($1, $2, $3, $4)';
-  pool.query(queryString, [req.body.rank, req.body.artist, req.body.track, req.body.published]).then ((results)=>{
+  pool.query(queryString, [req.body.rank, req.body.artist, req.body.track, req.body.published]).then((results) => {
     console.log('track added to db');
     res.sendStatus(201);
-  }).catch((err)=>{
+  }).catch((err) => {
     console.log(err);
     res.sendStatus(500);
   })
